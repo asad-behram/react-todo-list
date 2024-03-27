a036e6ac45fa85f48298bda19fbd1d1e50db952f
asad <asadbehram17@gmail.com>
2024-03-27 15:20:36 +0500

    completed front end

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react"
 import { Header } from "./components/Header"
+import { Tasks } from "./components/Tasks/Index"
 
 function App() {
+  // const [tasks, setTask] = useState([]);
+
+  // function addTask(taskTitle) {
+  //   setTask([
+  //     ...tasks,
+  //     {
+  //       id: crypto.randomUUID()
+  //     }
+  //   ])
+  // }
+
   return (
+    <>
     <Header/>
+    <Tasks/>
+    </>
   )
 }
 
